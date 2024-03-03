2af17b4a13d73418dcab1e7eb0e9586d68f58e36
VladyBoiko <bunkerwlada@gmail.com>
2024-03-03 17:39:01 +0200

    First project commit

---
+++ Source/UE5CppRobotDreams/UE5CppRobotDreamsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5CppRobotDreamsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUE5CppRobotDreamsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUE5CppRobotDreamsGameMode();
+};
+
+
+
